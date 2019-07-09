26d264418e8f78dc7ae798b82c6dc9e4bbd4b5a2
Tarek Mostafa <tarekmostafag@gmail.com>
2019-07-09 09:31:38 +0200

    mongo and be bindings works

--- dockerfile
+++ dockerfile
@@ -1,7 +1,5 @@
 FROM node:10
 
-RUN mkdir -p /usr/src/app
-
 WORKDIR /usr/src/app
 
 COPY package*.json ./
@@ -10,6 +8,6 @@ RUN npm install
 
 COPY . .
 
-EXPOSE 3000
+# EXPOSE 3000
 
 CMD ["npm", "run", "watch"]
--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ import { MONGODB_URI, SESSION_SECRET } from "./util/secrets";
 // import '@tensorflow/tfjs-node';
 
 
-import * as faceapi from 'face-api.js';
+// import * as faceapi from 'face-api.js';
 
 
 const storage = multer.diskStorage({
@@ -38,19 +38,14 @@ const MongoStore = mongo(session);
 // Controllers (route handlers)
 import * as homeController from "./controllers/home";
 import * as uploadController from "./controllers/upload_image";
-import * as userController from "./controllers/user";
-import * as apiController from "./controllers/api";
-import * as contactController from "./controllers/contact";
 
-
-// API keys and Passport configuration
-import * as passportConfig from "./config/passport";
 
 // Create Express server
 const app = express();
 
 // Connect to MongoDB
-const mongoUrl = MONGODB_URI;
+// const mongoUrl = MONGODB_URI;
+const mongoUrl = "mongodb://mongo:27017/test";
 (<any>mongoose).Promise = bluebird;
 
 mongoose.connect(mongoUrl, { useNewUrlParser: true} ).then(
@@ -110,35 +105,7 @@ app.use(
  */
 app.get("/", homeController.index);
 app.all("/upload", upload, uploadController.upload);
-app.get("/login", userController.getLogin);
-app.post("/login", userController.postLogin);
-app.get("/logout", userController.logout);
-app.get("/forgot", userController.getForgot);
-app.post("/forgot", userController.postForgot);
-app.get("/reset/:token", userController.getReset);
-app.post("/reset/:token", userController.postReset);
-app.get("/signup", userController.getSignup);
-app.post("/signup", userController.postSignup);
-app.get("/contact", contactController.getContact);
-app.post("/contact", contactController.postContact);
-app.get("/account", passportConfig.isAuthenticated, userController.getAccount);
-app.post("/account/profile", passportConfig.isAuthenticated, userController.postUpdateProfile);
-app.post("/account/password", passportConfig.isAuthenticated, userController.postUpdatePassword);
-app.post("/account/delete", passportConfig.isAuthenticated, userController.postDeleteAccount);
-app.get("/account/unlink/:provider", passportConfig.isAuthenticated, userController.getOauthUnlink);
 
-/**
- * API examples routes.
- */
-app.get("/api", apiController.getApi);
-app.get("/api/facebook", passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getFacebook);
 
-/**
- * OAuth authentication routes. (Sign in)
- */
-app.get("/auth/facebook", passport.authenticate("facebook", { scope: ["email", "public_profile"] }));
-app.get("/auth/facebook/callback", passport.authenticate("facebook", { failureRedirect: "/login" }), (req, res) => {
-  res.redirect(req.session.returnTo || "/");
-});
 
 export default app;
